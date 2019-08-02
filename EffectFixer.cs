c2abeab771fe1309ec26e3c481995f508f436625
Mohammad Ghaemi <seyedmohammadghaem@gmail.com>
2019-08-02 07:27:09 -0400

    Always enable fury when equipped

---
+++ EffectFixer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Reflection;
+using System;
+using HutongGames.PlayMaker.Actions;
+using ModCommon;
+using ModCommon.Util;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Logger = Modding.Logger;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace FuryAlwaysOn
+{
+    internal class EffectFixer : MonoBehaviour
+    {
+        PlayMakerFSM _fsm;
+        private void Start()
+        {
+            StartCoroutine(WaitForPlayer());
+        }
+        IEnumerator WaitForPlayer()
+        {
+            yield return new WaitForSeconds(5f); //Waits for Hero to be loaded
+            _fsm = GameObject.Find("Charm Effects").LocateMyFSM("Fury");
+            _fsm.ChangeTransition("Check HP", "CANCEL", "Get Ref"); //Ensures the fsm does not cancel when HP is not at 1
+            _fsm.RemoveTransition("Activate", "HERO DAMAGED"); //Ensures hp is not checked when hit
+            _fsm.RemoveTransition("Activate", "HERO HEALED"); //Ensures hp is not checked when healed
+            _fsm.RemoveTransition("Activate", "HERO HEALED FULL"); //Ensures hp is not checked when at bench
+            _fsm.RemoveTransition("Activate", "ADD BLUE HEALTH"); //Ensures hp is not checked when lifeblood is received
+
+            while (true)
+            {
+                if (PlayerData.instance.equippedCharm_6 && _fsm.ActiveStateName == "Idle") //Turns on fury when equipped
+                {
+                    _fsm.SendEvent("HERO DAMAGED"); //Starts
+                }
+                if (!PlayerData.instance.equippedCharm_6 && _fsm.ActiveStateName == "Activate") //Turns of fury when not equipped
+                {
+                    _fsm.SetState("Deactivate");
+                }
+                yield return null;
+            }
+        }
+        public static void Log(object o)
+        {
+            Logger.Log("[EffectFixer] " + o);
+        }
+    }
+}
---
+++ FuryAlwaysOn.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Modding;
+using JetBrains.Annotations;
+using ModCommon;
+using MonoMod.RuntimeDetour;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+using UObject = UnityEngine.Object;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FuryAlwaysOn
+{
+    [UsedImplicitly]
+    public class FuryAlwaysOn : Mod, ITogglableMod
+    {
+        public static FuryAlwaysOn Instance;
+
+        public override string GetVersion()
+        {
+            return "0.0.0.0";
+        }
+
+        public override void Initialize()
+        {
+            Instance = this;
+            Log("Initalizing.");
+
+            Unload(); //Ensures two instances of this mod are not working at the same time
+            ModHooks.Instance.AfterSavegameLoadHook += AfterSaveGameLoad; //Runs when a save file is chosen
+            ModHooks.Instance.NewGameHook += AddComponent; 
+        }
+
+        private void AfterSaveGameLoad(SaveGameData data) => AddComponent();
+
+        private void AddComponent()
+        {
+            GameManager.instance.gameObject.AddComponent<EffectFixer>(); //Begins class EffectFixer when a new save file loads
+        }
+
+        public void Unload()
+        {
+            ModHooks.Instance.AfterSavegameLoadHook -= AfterSaveGameLoad;
+            ModHooks.Instance.NewGameHook -= AddComponent;
+
+            var x = GameManager.instance?.gameObject.GetComponent<EffectFixer>();
+            if (x == null) return;
+            UObject.Destroy(x);
+        }
+    }
+}
