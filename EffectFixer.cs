4b7d90c9d32131a7231bc972b1b7959a82024bb4
Araraura <25017422+Araraura@users.noreply.github.com>
2022-01-10 21:19:44 +0200

    Cleanup code (Replaces spaces with tab indents)

--- EffectFixer.cs
+++ EffectFixer.cs
@@ -5,49 +5,52 @@ using USceneManager = UnityEngine.SceneManagement.SceneManager;
 
 namespace FuryAlwaysOn
 {
-    internal class EffectFixer : MonoBehaviour
-    {
-        PlayMakerFSM _fsm;
-        private void Start()
-        {
-            StartCoroutine(WaitForPlayer());
-        }
-        IEnumerator WaitForPlayer()
-        {
-            yield return new WaitWhile(() => HeroController.instance == null); //Waits for Hero to be loaded
-            yield return null;
-            _fsm = GameObject.Find("Charm Effects").LocateMyFSM("Fury");
-            _fsm.ChangeTransition("Check HP", "CANCEL", "Get Ref"); //Ensures the fsm does not cancel when HP is not at 1
-            _fsm.RemoveTransition("Activate", "HERO DAMAGED"); //Ensures hp is not checked when hit
-            _fsm.RemoveTransition("Activate", "HERO HEALED"); //Ensures hp is not checked when healed
-            _fsm.RemoveTransition("Activate", "HERO HEALED FULL"); //Ensures hp is not checked when at bench
-            _fsm.RemoveTransition("Activate", "ADD BLUE HEALTH"); //Ensures hp is not checked when lifeblood is received
-            _fsm.RemoveAction("Activate",21);
-            _fsm.RemoveAction("Activate",20);
-            _fsm.RemoveAction("Activate", 2);
-            _fsm.RemoveAction("Activate", 1);
-            _fsm.RemoveAction("Activate", 0);
-            HeroController.instance.grubberFlyBeamPrefabL = HeroController.instance.grubberFlyBeamPrefabL_fury;
-            HeroController.instance.grubberFlyBeamPrefabR = HeroController.instance.grubberFlyBeamPrefabR_fury;
-            HeroController.instance.grubberFlyBeamPrefabU = HeroController.instance.grubberFlyBeamPrefabU_fury;
-            HeroController.instance.grubberFlyBeamPrefabD = HeroController.instance.grubberFlyBeamPrefabD_fury;
+	internal class EffectFixer : MonoBehaviour
+	{
+		PlayMakerFSM _fsm;
 
-            while (true)
-            {
-                if (PlayerData.instance.equippedCharm_6 && _fsm.ActiveStateName == "Idle") //Turns on fury when equipped
-                {
-                    _fsm.SendEvent("HERO DAMAGED"); //Starts
-                }
-                if (!PlayerData.instance.equippedCharm_6 && _fsm.ActiveStateName == "Activate") //Turns of fury when not equipped
-                {
-                    _fsm.SetState("Deactivate");
-                }
-                yield return null;
-            }
-        }
-        public static void Log(object o)
-        {
-            Logger.Log("[EffectFixer] " + o);
-        }
-    }
+		private void Start()
+		{
+			StartCoroutine(WaitForPlayer());
+		}
+
+		IEnumerator WaitForPlayer()
+		{
+			yield return new WaitWhile(() => HeroController.instance == null); //Waits for Hero to be loaded
+			yield return null;
+			_fsm = GameObject.Find("Charm Effects").LocateMyFSM("Fury");
+			_fsm.ChangeTransition("Check HP", "CANCEL", "Get Ref"); //Ensures the fsm does not cancel when HP is not at 1
+			_fsm.RemoveTransition("Activate", "HERO DAMAGED");      //Ensures hp is not checked when hit
+			_fsm.RemoveTransition("Activate", "HERO HEALED");       //Ensures hp is not checked when healed
+			_fsm.RemoveTransition("Activate", "HERO HEALED FULL");  //Ensures hp is not checked when at bench
+			_fsm.RemoveTransition("Activate", "ADD BLUE HEALTH");   //Ensures hp is not checked when lifeblood is received
+			_fsm.RemoveAction("Activate", 21);
+			_fsm.RemoveAction("Activate", 20);
+			_fsm.RemoveAction("Activate", 2);
+			_fsm.RemoveAction("Activate", 1);
+			_fsm.RemoveAction("Activate", 0);
+			HeroController.instance.grubberFlyBeamPrefabL = HeroController.instance.grubberFlyBeamPrefabL_fury;
+			HeroController.instance.grubberFlyBeamPrefabR = HeroController.instance.grubberFlyBeamPrefabR_fury;
+			HeroController.instance.grubberFlyBeamPrefabU = HeroController.instance.grubberFlyBeamPrefabU_fury;
+			HeroController.instance.grubberFlyBeamPrefabD = HeroController.instance.grubberFlyBeamPrefabD_fury;
+
+			while (true)
+			{
+				if (PlayerData.instance.equippedCharm_6 && _fsm.ActiveStateName == "Idle") //Turns on fury when equipped
+				{
+					_fsm.SendEvent("HERO DAMAGED"); //Starts
+				}
+				if (!PlayerData.instance.equippedCharm_6 && _fsm.ActiveStateName == "Activate") //Turns of fury when not equipped
+				{
+					_fsm.SetState("Deactivate");
+				}
+				yield return null;
+			}
+		}
+
+		public static void Log(object o)
+		{
+			Logger.Log("[EffectFixer] " + o);
+		}
+	}
 }
--- FuryAlwaysOn.cs
+++ FuryAlwaysOn.cs
@@ -4,40 +4,41 @@ using UObject = UnityEngine.Object;
 
 namespace FuryAlwaysOn
 {
-    public class FuryAlwaysOn : Mod, ITogglableMod
-    {
-        public static FuryAlwaysOn Instance;
+	public class FuryAlwaysOn : Mod, ITogglableMod
+	{
+		public static FuryAlwaysOn Instance;
 
-        public override string GetVersion()
-        {
-            return "0.0.0.0";
-        }
+		public override string GetVersion()
+		{
+			return "0.0.0.0";
+		}
 
-        public override void Initialize()
-        {
-            Instance = this;
-            Log("Initializing.");
+		public override void Initialize()
+		{
+			Instance = this;
+			Log("Initializing.");
 
-            Unload(); //Ensures two instances of this mod are not working at the same time
-            ModHooks.AfterSavegameLoadHook += AfterSaveGameLoad; //Runs when a save file is chosen
-            ModHooks.NewGameHook += AddComponent;
-        }
+			Unload();                                            //Ensures two instances of this mod are not working at the same time
+			ModHooks.AfterSavegameLoadHook += AfterSaveGameLoad; //Runs when a save file is chosen
+			ModHooks.NewGameHook           += AddComponent;
+		}
 
-        private void AfterSaveGameLoad(SaveGameData data) => AddComponent();
+		private void AfterSaveGameLoad(SaveGameData data) => AddComponent();
 
-        private void AddComponent()
-        {
-            GameManager.instance.gameObject.AddComponent<EffectFixer>(); //Begins class EffectFixer when a new save file loads
-        }
+		private void AddComponent()
+		{
+			GameManager.instance.gameObject.AddComponent<EffectFixer>(); //Begins class EffectFixer when a new save file loads
+		}
 
-        public void Unload()
-        {
-            ModHooks.AfterSavegameLoadHook -= AfterSaveGameLoad;
-            ModHooks.NewGameHook -= AddComponent;
+		public void Unload()
+		{
+			ModHooks.AfterSavegameLoadHook -= AfterSaveGameLoad;
+			ModHooks.NewGameHook           -= AddComponent;
 
-            var x = GameManager.instance?.gameObject.GetComponent<EffectFixer>();
-            if (x == null) return;
-            UObject.Destroy(x);
-        }
-    }
+			var x = GameManager.instance?.gameObject.GetComponent<EffectFixer>();
+			if (x == null)
+				return;
+			UObject.Destroy(x);
+		}
+	}
 }
